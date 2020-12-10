a0402dce5970d3b2a26c4646ea9632d6e96dd957
Tilman Raendchen <traendchen@outlook.com>
2020-12-10 18:59:28 +0000

    Remove redundant lines

--- src/scripts/objects/settings/variations/Slider.js
+++ src/scripts/objects/settings/variations/Slider.js
@@ -1,8 +1,3 @@
-const COLOR_PRIMARY = 0x4e342e;
-const COLOR_LIGHT = 0x7b5e57;
-const COLOR_DARK = 0x260e04;
-
-import * as Phaser from "phaser";
 import { Slider as RexSlider } from "phaser3-rex-plugins/templates/ui/ui-components.js";
 
 import Setting from "../Setting.js";
