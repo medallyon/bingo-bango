55d85400020cfe08136269fd751e7772b77f4cb4
Tilman Raendchen <traendchen@outlook.com>
2021-01-14 08:37:55 +0000

    Ensure cookie's value is URI-decoded

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -98,13 +98,13 @@ class Bingo extends Phaser.Game
 		this.announcer = new Voicepack();
 		this.connection = new ConnectionHandler(this);
 
-		this.user = JSON.parse((name => // https://stackoverflow.com/a/15724300/4672263
+		this.user = JSON.parse(decodeURI((name => // https://stackoverflow.com/a/15724300/4672263
 		{
 			const value = `; ${document.cookie}`;
 			const parts = value.split(`; ${name}=`);
 			if (parts.length === 2)
 				return parts.pop().split(";").shift();
-		})("user"));
+		})("user")));
 
 		console.log(`Hello there, ${this.user.username}#${this.user.tag}`);
 	}
